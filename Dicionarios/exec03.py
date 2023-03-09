fb7c5aeb0c14772e9a050acce5c38c3e56dd7bb1
kelver <kelverwt@gmail.com>
2023-03-09 11:22:44 -0300

    dados pessoa

---
+++ Dicionarios/exec03.py
@@ -0,0 +1,5 @@
+# Crie um dicionário que representa uma pessoa com as seguintes informações: nome, idade, cidade e telefone
+# mostre o nome, telefone.
+
+pessoa = {'nome': 'Maria', 'idade': 34, 'cidade': 'Natal', 'telefone': 123456789}
+print(f'Nome: {pessoa["nome"]} \nTelefone: {pessoa["telefone"]}')
